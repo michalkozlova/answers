7fba9b04eac25d8c15db5a48b71737769be19649
michal <michalkozlova@gmail.com>
2019-05-20 15:01:47 +0300

    date and time pickers added

--- app/src/main/java/michal/edu/answers/LeaveFeedback/StartFeedbackFragment.java
+++ app/src/main/java/michal/edu/answers/LeaveFeedback/StartFeedbackFragment.java
@@ -1,35 +1,33 @@
 package michal.edu.answers.LeaveFeedback;
 
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ServerValue;
 
-import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Map;
 
 import michal.edu.answers.DataSource;
-import michal.edu.answers.MainActivity;
 import michal.edu.answers.Models.Store;
 import michal.edu.answers.R;
 
@@ -37,22 +35,17 @@ import michal.edu.answers.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StartFeedbackFragment extends Fragment {
-
-    private final static String PREFERENCE_FILE = "michal.edu.answers.PREFERENCE_FILE_KEY";
+public class StartFeedbackFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private DataSource dataSource = DataSource.getInstance();
     private ImageView cardImage;
-    private TextView firstLetter, tvStoreNameBranchName;
-    private EditText etDate, etHour, etMin;
+    private TextView firstLetter, tvStoreNameBranchName, tvFullDate, tvFullTime;
     private Button btnNext;
-    private Spinner spinnerMonth;
     private View bottomBar;
     private Store thisStore;
-    private String thisBranchName;
+    private String thisBranchName, fullDate, fullTime, fullDateAndTime, userID;
     private long timestamp;
     private SharedPreferences sharedPref;
-    private String userID;
 
     public static StartFeedbackFragment newInstance(Store store, String branchName) {
 
@@ -70,8 +63,6 @@ public class StartFeedbackFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_start_feedback, container, false);
 
-        sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-
         try {
             userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
         }catch (NullPointerException e){
@@ -79,28 +70,37 @@ public class StartFeedbackFragment extends Fragment {
             userID = "Anonymous";
         }
 
+        getFullDateAndTime();
+
         setInitialView(v);
 
-        getTime();
+        tvFullDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
 
+        tvFullTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!isHourValid()){
-                    return;
-                }else {
                     timeChanged();
-                }
-
-                saveInfoToSharedPref();
-
-                getActivity()
-                        .getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.container, FirstSectionFragment.newInstance(thisStore, thisBranchName))
-                        .disallowAddToBackStack()
-                        .commit();
+//TODO: take of comment
+//                saveInfoToSharedPref();
+//
+//                getActivity()
+//                        .getSupportFragmentManager()
+//                        .beginTransaction()
+//                        .replace(R.id.container, FirstSectionFragment.newInstance(thisStore, thisBranchName))
+//                        .disallowAddToBackStack()
+//                        .commit();
             }
         });
 
@@ -108,15 +108,25 @@ public class StartFeedbackFragment extends Fragment {
     }
 
     private void setInitialView(View v){
-        spinnerMonth = v.findViewById(R.id.spinnerMonth);
+        //spinnerMonth = v.findViewById(R.id.spinnerMonth);
         cardImage = v.findViewById(R.id.cardImage);
         firstLetter = v.findViewById(R.id.firstLetter);
         tvStoreNameBranchName = v.findViewById(R.id.tvStoreNameBranchName);
         btnNext = v.findViewById(R.id.btnNext);
         bottomBar = getActivity().getWindow().findViewById(R.id.navigation);
-        etDate = v.findViewById(R.id.etDate);
-        etHour = v.findViewById(R.id.etHour);
-        etMin = v.findViewById(R.id.etMin);
+//        etDate = v.findViewById(R.id.etDate);
+//        etHour = v.findViewById(R.id.etHour);
+//        etMin = v.findViewById(R.id.etMin);
+        tvFullDate = v.findViewById(R.id.tvFullDate);
+        tvFullTime = v.findViewById(R.id.tvFullTime);
+
+        fullDate = fullDateAndTime.substring(6, 16);
+        tvFullDate.setText(fullDate);
+
+        fullTime = fullDateAndTime.substring(0, 5);
+        tvFullTime.setText(fullTime);
+
+        sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
 
         thisStore = (Store) getArguments().getSerializable("store");
         thisBranchName = (String) getArguments().getSerializable("branchName");
@@ -124,54 +134,80 @@ public class StartFeedbackFragment extends Fragment {
         dataSource.setStoreLogo(thisStore, cardImage, firstLetter, getContext());
         tvStoreNameBranchName.setText(thisStore.getStoreName() + " / " + thisBranchName);
 
-        ArrayAdapter<CharSequence> monthAdapter = ArrayAdapter.createFromResource(getContext(), R.array.months, R.layout.spinner_item_month);
-        monthAdapter.setDropDownViewResource(R.layout.spinner_item_month);
-        spinnerMonth.setAdapter(monthAdapter);
+//        ArrayAdapter<CharSequence> monthAdapter = ArrayAdapter.createFromResource(getContext(), R.array.months, R.layout.spinner_item_month);
+//        monthAdapter.setDropDownViewResource(R.layout.spinner_item_month);
+//        spinnerMonth.setAdapter(monthAdapter);
 
         bottomBar.clearAnimation();
         bottomBar.animate().translationY(bottomBar.getHeight()).setDuration(400);
 
     }
 
-    private String hour(){
-        return etHour.getText().toString();
-    }
-    private String min(){
-        return etMin.getText().toString();
-    }
-    private String day(){
-        return etDate.getText().toString();
-    }
-
-    private void getTime(){
+    private void getFullDateAndTime(){
         timestamp = System.currentTimeMillis();
         System.out.println(timestamp);
 
         SimpleDateFormat formatter = new SimpleDateFormat("kk:mm dd/MM/yyyy");
-        String fullTime = formatter.format(new Date(timestamp));
-        System.out.println(fullTime);
+        fullDateAndTime = formatter.format(new Date(timestamp));
+        System.out.println("Full date and Time: " + fullDateAndTime);
+    }
 
-        String hour = fullTime.substring(0, 2);
-        etHour.setText(hour);
+    private int day(){
+        String dayString = fullDateAndTime.substring(6, 8);
+        return Integer.valueOf(dayString);
+    }
 
-        String min = fullTime.substring(3, 5);
-        etMin.setText(min);
+    private int month(){
+        String monthString = fullDateAndTime.substring(9, 11);
+        return Integer.valueOf(monthString)-1;
+    }
 
-        String day = fullTime.substring(6, 8);
-        etDate.setText(day);
+    private int year(){
+        String yearString = fullDateAndTime.substring(12, 16);
+        return Integer.valueOf(yearString);
+    }
 
-        String months = fullTime.substring(9, 11);
-        Integer mon = Integer.valueOf(months);
-        spinnerMonth.setSelection(mon-1);
+    private void showDatePickerDialog() {
+        new DatePickerDialog(getContext(), this, year(), month(), day()).show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Toast.makeText(getContext(), "Done " + dayOfMonth + "/" + (month+1) + "/" + year, Toast.LENGTH_SHORT).show();
+        fullDate = dayOfMonth + "/" + (month+1) + "/" + year;
+        tvFullDate.setText(fullDate);
+    }
+
+
+    private int hour(){
+        String hourString = fullDateAndTime.substring(0, 2);
+        return Integer.valueOf(hourString);
+    }
+    private int min(){
+        String minString = fullDateAndTime.substring(3, 5);
+        return Integer.valueOf(minString);
+    }
+
+    private void showTimePickerDialog() {
+        new TimePickerDialog(getContext(), this, hour(), min(), true).show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Toast.makeText(getContext(), "Done: " + hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
+        fullTime = hourOfDay + ":" + minute;
+        tvFullTime.setText(fullTime);
     }
 
     private long timeChanged(){
+        //TODO: fix the method
         System.out.println(timestamp + " before time was changed");
-        long month = spinnerMonth.getSelectedItemId() + 1;
+        //long month = spinnerMonth.getSelectedItemId() + 1;
 
         SimpleDateFormat formatter = new SimpleDateFormat("kk:mm dd/MM/yyy");
+
         try {
-            Date mDate = formatter.parse(hour() + ":" + min() + " " + day() + "/" + month + "/2019");
+            Date mDate = formatter.parse(fullTime + " " + fullDate);
             timestamp = mDate.getTime();
             System.out.println(timestamp + " after time was changed");
             String newFullTime = formatter.format(new Date(timestamp));
@@ -183,17 +219,6 @@ public class StartFeedbackFragment extends Fragment {
         return timestamp;
     }
 
-    private boolean isHourValid() {
-        int x = Integer.valueOf(hour());
-
-        if (x < 0 || x > 24){
-            etHour.setError("Hour is not correct");
-            return false;
-        } else {
-            return true;
-        }
-    }
-
     private void saveInfoToSharedPref(){
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString("customerID", userID);
@@ -202,5 +227,4 @@ public class StartFeedbackFragment extends Fragment {
         editor.putLong("timestamp", timestamp);
         editor.apply();
     }
-
 }
